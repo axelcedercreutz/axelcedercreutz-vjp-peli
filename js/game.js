970874d4d8da038c6a82df42c21b60f01495a624
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-04-18 00:05:34 +0300

    game kinda works

--- js/game.js
+++ js/game.js
@@ -16,7 +16,9 @@ var rightKey;
 var pressTime;
 
 var health = 100;
-var text = 0;
+var text;
+var score = 0;
+var scoreText;
 
 function create() {
 
@@ -38,8 +40,9 @@ function create() {
     // sprite1.animations.play('run', 10, true);
     sprite2.animations.play('run', 10, true);
 
-
-    text = game.add.text(game.world.centerX, game.world.centerY, 'Health: 100', { font: "64px Arial", fill: "#ffffff", align: "center" });
+    scoreText = game.add.text(game.world.centerX, game.world.centerY + 40, 'Score: 0', { font: "32px Arial", fill: "#ffffff", align: "center" });
+    scoreText.anchor.setTo(0.5, 0.5);
+    text = game.add.text(game.world.centerX, game.world.centerY, 'Health: 100', { font: "32px Arial", fill: "#ffffff", align: "center" });
     text.anchor.setTo(0.5, 0.5);
 
     game.physics.arcade.enable([ enemies, sprite2 ], Phaser.Physics.ARCADE);
@@ -69,78 +72,85 @@ function resurrect() {
 
 
 function updateCounter() {
-    health --;
-    text.setText('Counter: ' + health);
+    if(health > 0) {
+        health --;
+    }
+    text.setText('Health: ' + health);
+    scoreText.setText('Score: ' + score);
 
 }
 
 
 function update() {
-    if(upKey.isDown){
-        if(pressTime != upKey.timeDown) {
-            for (var i = 0; i < enemies.children.length; i++) {
-                enemies.children[i].position.y += 100;
+    if(health > 0) {
+        if(upKey.isDown){
+            if(pressTime != upKey.timeDown) {
+                for (var i = 0; i < enemies.children.length; i++) {
+                    enemies.children[i].position.y += 100;
+                }
+                pressTime = upKey.timeDown;
+                resurrect();
+                if(health <= 99) {
+                    health = health + 2;
+                }
+                score ++;
+                updateCounter();
             }
-            pressTime = upKey.timeDown;
-            resurrect();
-            if(health <= 99) {
-                health = health + 2;
-            }
-            updateCounter();
         }
-    }
-    if (leftKey.isDown){
-        if(pressTime != leftKey.timeDown) {
-            if(sprite2.x <= 150) {
-                sprite2.x = 0;
-                for (var i = 0; i < enemies.children.length; i++) {
-                    enemies.children[i].position.y += 100;
+        if (leftKey.isDown){
+            if(pressTime != leftKey.timeDown) {
+                if(sprite2.x <= 150) {
+                    sprite2.x = 0;
+                    for (var i = 0; i < enemies.children.length; i++) {
+                        enemies.children[i].position.y += 100;
+                    }
                 }
-            }
-            else{
-                sprite2.x = sprite2.x - 150;
-                for (var i = 0; i < enemies.children.length; i++) {
-                    enemies.children[i].position.y += 100;
-                }
+                else{
+                    sprite2.x = sprite2.x - 150;
+                    for (var i = 0; i < enemies.children.length; i++) {
+                        enemies.children[i].position.y += 100;
+                    }
 
-            }
-            pressTime = leftKey.timeDown;
-            resurrect();
-            if(health <= 99) {
-                health = health + 2;
-            }
-            updateCounter();
-        }
-    }
-    else if (rightKey.isDown) {
-        if(pressTime != rightKey.timeDown) {
-            if(sprite2.x >= 150){
-                sprite2.x = 300
-                for (var i = 0; i < enemies.children.length; i++) {
-                    enemies.children[i].position.y += 100;
-                 }
-            }
-            else {
-                sprite2.x = sprite2.x + 150;
-                for (var i = 0; i < enemies.children.length; i++) {
-                    enemies.children[i].position.y += 100;
                 }
+                pressTime = leftKey.timeDown;
+                resurrect();
+                if(health <= 99) {
+                    health = health + 2;
+                }
+                score ++;
+                updateCounter();
             }
-            pressTime = rightKey.timeDown;
-            resurrect();
-            if(health >= 99) {
-                health = 101;
+        }
+        else if (rightKey.isDown) {
+            if(pressTime != rightKey.timeDown) {
+                if(sprite2.x >= 150){
+                    sprite2.x = 300
+                    for (var i = 0; i < enemies.children.length; i++) {
+                        enemies.children[i].position.y += 100;
+                     }
+                }
+                else {
+                    sprite2.x = sprite2.x + 150;
+                    for (var i = 0; i < enemies.children.length; i++) {
+                        enemies.children[i].position.y += 100;
+                    }
+                }
+                pressTime = rightKey.timeDown;
+                resurrect();
+                if(health >= 99) {
+                    health = 101;
+                }
+                score ++;
+                updateCounter();
             }
-            updateCounter();
+        }
+        for (var i = 0; i < enemies.children.length; i++) {
+            if(enemies.children[i].position.y >= 800) {
+                enemies.children[i].kill();
+            }
+            game.physics.arcade.overlap(enemies.children[i], sprite2, overlapHandler, null, this);
         }
     }
-    for (var i = 0; i < enemies.children.length; i++) {
-        if(enemies.children[i].position.y >= 800) {
-            enemies.children[i].kill();
-        }
-        game.physics.arcade.overlap(enemies.children[i], sprite2, overlapHandler, null, this);
-    }
-    // game.physics.arcade.overlap(enemies, sprite2, overlapHandler, null, this);
 }
 
 function overlapHandler (obj1, obj2) {
