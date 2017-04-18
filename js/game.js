93f6a91566d78a7c37a075d17d680240f9618de5
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-04-18 14:50:30 +0300

    adding comments

--- js/game.js
+++ js/game.js
@@ -1,18 +1,22 @@
+//creates the game
 var game = new Phaser.Game(400, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
+//defines the preloaded things
 function preload() {
     game.load.spritesheet('player', 'assets/fish-sprite.png', 80, 80, 8);
     game.load.spritesheet('goodFish', 'assets/good_fish.png', 35, 32, 5);
     game.load.spritesheet('button', 'assets/button_sprite_sheet.png', 193, 71);
 }
-
+//global variables for the players
 var enemies;
-var sprite2;
+var player;
 
+//global variables for the keys
 var leftKey;
 var rightKey;
 var pressTime;
 
+//global variables for the text that's always on the screen
 var health = 100;
 var text;
 var score = 0;
@@ -20,12 +24,25 @@ var scoreText;
 var level = 1;
 var levelText;
 
+//global variables for the timer
 var timer = Phaser.Timer.SECOND;
 var timerRun;
 
+//global variable for the button
 var button;
 
+//global variable for the update to only run the gameOver-function once
 var deathCount = 0;
+
+
+//This is where the game is initially created.
+//It starts with activating the physics from Phaser.js,
+//then adds the background color, creates the enemy group and creates the first enemy,
+//creates our player. Then it sets the size of the player and the enemies and
+//starts the animation of the player.
+//It writes all the text that is rendered to the screen all the time and sets the position for it.
+//It adds the game physics to the enemies and the player.
+//It adds to the global variables the keys and starts the timer.
 
 function create() {
 
@@ -35,13 +52,13 @@ function create() {
 
     enemies = game.add.group();
     enemies.createMultiple(1, 'goodFish', 0, false);
-    sprite2 = game.add.sprite(150, 470, 'player');
+    player = game.add.sprite(150, 470, 'player');
 
-    sprite2.scale.setTo(1.2,1.2);
+    player.scale.setTo(1.2,1.2);
     enemies.scale.set(1.2, 1.2);
 
-    sprite2.animations.add('run');
-    sprite2.animations.play('run', 10, true);
+    player.animations.add('run');
+    player.animations.play('run', 10, true);
 
     scoreText = game.add.text(game.world.centerX, game.world.centerY + 40, 'Score: 0', { font: "32px Arial", fill: "#ffffff", align: "center" });
     scoreText.anchor.setTo(0.5, 0.5);
@@ -50,20 +67,20 @@ function create() {
     levelText = game.add.text(game.world.centerX, 20, 'Level: 1', { font: "32px Arial", fill: "#ffffff", align: "center" });
     levelText.anchor.setTo(0.5, 0.5);
 
-    game.physics.arcade.enable([ enemies, sprite2 ], Phaser.Physics.ARCADE);
+    game.physics.arcade.enable([ enemies, player ], Phaser.Physics.ARCADE);
 
     upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
     timerRun = game.time.events.loop(timer, updateCounter, this);
-    timerRun;
 }
-function resurrect() {
 
-    //  Get a dead item - The Group was seeded with 5 'dead' items,
-    //  so those will be re-used first and then it will start
-    //  creating new ones using the following arguments:
+//ressurect creates the enemies. It chooses by random which of the three
+//x-positions it starts at, adds the image and the frame and then creates the enemy.
+//then it adds the physics for the enemy.
+
+function resurrect() {
 
     var x = Math.floor(Math.random() * 3) * 150;
     var y = 0;
@@ -71,10 +88,13 @@ function resurrect() {
     var frame = game.rnd.between(0, 36);
 
     enemies.getFirstDead(true, x, y, key, frame);
-    game.physics.arcade.enable([ enemies, sprite2 ], Phaser.Physics.ARCADE);
+    game.physics.arcade.enable([ enemies, player ], Phaser.Physics.ARCADE);
 
 }
 
+// This function adds takes away health when timer runs and sets the new text.
+// It also does this when a key has been pressed so that the health, score and level
+// are correct.
 
 function updateCounter() {
     if(health > 0) {
@@ -96,7 +116,7 @@ function update() {
                 }
                 pressTime = upKey.timeDown;
                 resurrect();
-                if(health <= (100 - level)) {
+                if(health <= 99) {
                     health = health + 2;
                 }
                 score ++;
@@ -117,14 +137,14 @@ function update() {
         }
         if (leftKey.isDown){
             if(pressTime != leftKey.timeDown) {
-                if(sprite2.x <= 150) {
-                    sprite2.x = 0;
+                if(player.x <= 150) {
+                    player.x = 0;
                     for (var i = 0; i < enemies.children.length; i++) {
                         enemies.children[i].position.y += 100;
                     }
                 }
                 else{
-                    sprite2.x = sprite2.x - 150;
+                    player.x = player.x - 150;
                     for (var i = 0; i < enemies.children.length; i++) {
                         enemies.children[i].position.y += 100;
                     }
@@ -154,14 +174,14 @@ function update() {
         }
         else if (rightKey.isDown) {
             if(pressTime != rightKey.timeDown) {
-                if(sprite2.x >= 150){
-                    sprite2.x = 300
+                if(player.x >= 150){
+                    player.x = 300
                     for (var i = 0; i < enemies.children.length; i++) {
                         enemies.children[i].position.y += 100;
                      }
                 }
                 else {
-                    sprite2.x = sprite2.x + 150;
+                    player.x = player.x + 150;
                     for (var i = 0; i < enemies.children.length; i++) {
                         enemies.children[i].position.y += 100;
                     }
@@ -194,7 +214,7 @@ function update() {
             if(enemies.children[i].position.y >= 800) {
                 enemies.children[i].kill();
             }
-            game.physics.arcade.overlap(enemies.children[i], sprite2, gameOver, null, this);
+            game.physics.arcade.overlap(enemies.children[i], player, gameOver, null, this);
         }
     }
     else {
@@ -219,26 +239,26 @@ function gameOver() {
         enemies.children[i].kill();
     }
 
-    sprite2.kill();
+    player.kill();
 
     button = game.add.button(game.world.centerX - 95, 400, 'button', reset, this, 2, 1, 0);
 }
 function reset() {
-    sprite2 = game.add.sprite(150, 470, 'player');
+    player = game.add.sprite(150, 470, 'player');
     enemies = game.add.group();
     enemies.createMultiple(1, 'goodFish', 0, false);
 
-    sprite2.scale.setTo(1.2,1.2);
+    player.scale.setTo(1.2,1.2);
     enemies.scale.set(1.2, 1.2);
 
-    sprite2.animations.add('run');
-    sprite2.animations.play('run', 10, true);
+    player.animations.add('run');
+    player.animations.play('run', 10, true);
 
     score = 0;
     health = 101;
     level = 1;
     updateCounter();
-    game.physics.arcade.enable([ enemies, sprite2 ], Phaser.Physics.ARCADE);
+    game.physics.arcade.enable([ enemies, player ], Phaser.Physics.ARCADE);
 
     game.stage.backgroundColor = '#124184';
 
