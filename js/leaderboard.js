bba1276083b2065820f6cc90b26aa504cc6b7c1f
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-05-03 11:34:37 +0200

    added a json-file for the database

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,3 +1,5 @@
-$(document).ready( function(){
-
+$(document).ready(function(){
+    $.getJSON('https://project-8145367811882739054.firebaseio.com/.json',function(res){
+        console.log(res);
+    });
 });
