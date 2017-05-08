b7c50f0fc74073551d819122e7a40399882f37a9
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2017-05-07 23:39:24 -0700

    cleaned the code a bit

--- js/game.js
+++ js/game.js
@@ -624,14 +624,6 @@ function jumpForward() {
     }
 }
 
-function toObject(names, values) {
-        var result = {};
-        for (var i = 0; i < names.length; i++) {
-             result[names[i]] = values[i];
-        }
-        return result;
-};
-
 // function that starts the game.
 
 function start() {
